37c5081d5c6cb505a8cd1ae79e4977b5414d9fd7
nick <mwauranicholas10@gmail.com>
2023-03-29 15:08:28 +0300

    changed folder

---
+++ app/controllers/hero_powers_controller.rb
@@ -0,0 +1,27 @@
+class HeroPowersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+      def index
+          hero_powers = HeroPower.all
+          render json: hero_powers
+      end
+  
+      def create
+          hero_power =  HeroPower.create!(hero_power_params)
+          render json: hero_power.to_json(except: [:created_at, :updated_at]), status: :created
+      end
+  
+  private
+  
+    def hero_power_params
+      params.permit(:strength, :power_id, :hero_id)
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
---
+++ app/controllers/powers_controller.rb
@@ -0,0 +1,39 @@
+class PowersController < ApplicationController
+    
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+    powers = Power.all
+    render json: powers.to_json(except: [:created_at, :updated_at])
+    end
+
+    def show
+        power = find_power
+        render json: power.to_json(except: [:created_at, :updated_at])
+    end
+
+    def update
+        power = find_power
+        power.update!(power_params)
+        render json: power.to_json(except: [:created_at, :updated_at]), status: :created
+    end
+
+    private
+
+    def find_power
+        Power.find(params[:id])
+    end
+
+    def power_params
+        params.permit(:name, :description)
+    end
+
+    def render_not_found_response
+        render json: { error: "power not found" }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+          render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+end
